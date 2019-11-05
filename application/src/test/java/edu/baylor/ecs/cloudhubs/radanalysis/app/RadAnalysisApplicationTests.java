a97a2ceb459885a01f4ed3706c48d8ae712ac258
Dipta Das <dipta670@gmail.com>
2019-11-05 14:54:16 -0600

    Added application test structure

---
+++ application/src/test/java/edu/baylor/ecs/cloudhubs/radanalysis/app/RadAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.baylor.ecs.cloudhubs.radanalysis.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
