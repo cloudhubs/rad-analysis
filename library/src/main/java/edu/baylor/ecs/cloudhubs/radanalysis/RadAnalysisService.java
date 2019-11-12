ca4bafa51a91ef4843f2d94d9f08d320879d3f33
Dipta Das <dipta670@gmail.com>
2019-11-12 17:13:31 -0600

    Refactor RadAnalysisService

--- library/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/RadAnalysisService.java
+++ library/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/RadAnalysisService.java
@@ -5,8 +5,10 @@ import edu.baylor.ecs.cloudhubs.rad.context.RadResponseContext;
 import edu.baylor.ecs.cloudhubs.rad.model.RestEntity;
 import edu.baylor.ecs.cloudhubs.rad.model.RestFlow;
 import edu.baylor.ecs.cloudhubs.rad.service.RestDiscoveryService;
+import edu.baylor.ecs.cloudhubs.radanalysis.context.ApiSecurityContext;
 import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisRequestContext;
 import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisResponseContext;
+import edu.baylor.ecs.cloudhubs.radanalysis.context.SecurityContextWrapper;
 import edu.baylor.ecs.seer.common.SeerSecurityNode;
 import edu.baylor.ecs.seer.common.context.SeerRequestContext;
 import edu.baylor.ecs.seer.common.context.SeerSecurityContext;
@@ -15,7 +17,6 @@ import edu.baylor.ecs.seer.common.security.SecurityRootMethod;
 import edu.baylor.ecs.seer.common.security.SeerSecurityConstraintViolation;
 import edu.baylor.ecs.seer.common.security.SeerSecurityEntityAccessViolation;
 import edu.baylor.ecs.seer.lweaver.service.ResourceService;
-import edu.baylor.ecs.seer.lweaver.service.SeerContextService;
 import edu.baylor.ecs.seer.lweaver.service.SeerMsSecurityContextService;
 import javassist.CtClass;
 import lombok.AllArgsConstructor;
@@ -29,7 +30,6 @@ import java.util.stream.Collectors;
 public class RadAnalysisService {
     private final ResourceService resourceService;
     private final RestDiscoveryService restDiscoveryService;
-    private final SeerContextService seerContextService;
     private final SeerMsSecurityContextService securityContextService;
 
     public RadAnalysisResponseContext generateRadAnalysisResponseContext(RadAnalysisRequestContext request) {
@@ -38,44 +38,76 @@ public class RadAnalysisService {
         RadResponseContext radResponseContext = generateRadResponseContext(request);
         responseContext.setRestFlowContext(radResponseContext.getRestFlowContext());
 
-        List<SeerSecurityContext> securityContexts = generateSeerSecurityContext(request);
+        // generate security contexts for microservices (original algorithm)
+        // wrap resource paths
+        List<SecurityContextWrapper> securityContexts = generateSeerSecurityContexts(request);
+        responseContext.setSecurityContexts(securityContexts);
+
+        // generate api security context
+        ApiSecurityContext apiSecurityContext = generateApiSecurityContext(
+                request,
+                securityContexts,
+                radResponseContext.getRestFlowContext().getRestFlows());
+
+        responseContext.setApiSecurityContext(apiSecurityContext);
+
+        return responseContext;
+    }
+
+    private RadResponseContext generateRadResponseContext(RadAnalysisRequestContext request) {
+        return restDiscoveryService.generateRadResponseContext(convertToRadRequestContext(request));
+    }
+
+    private List<SecurityContextWrapper> generateSeerSecurityContexts(RadAnalysisRequestContext request) {
+        List<SecurityContextWrapper> securityContexts = new ArrayList<>();
+
+        List<String> resourcePaths = resourceService.getResourcePaths(request.getPathToCompiledMicroservices());
+        for (String path : resourcePaths) {
+            List<CtClass> ctClasses = resourceService.getCtClasses(path, request.getOrganizationPath());
+            SeerRequestContext seerRequestContext = covertToSeerRequestContext(request);
+            SeerSecurityContext securityContext = securityContextService.getMsSeerSecurityContext(ctClasses, seerRequestContext);
+            securityContexts.add(new SecurityContextWrapper(path, securityContext));
+        }
+
+        return securityContexts;
+    }
+
+    private ApiSecurityContext generateApiSecurityContext(RadAnalysisRequestContext request, List<SecurityContextWrapper> securityContexts, List<RestFlow> restFlows) {
+        // prepare seer api security context
+        String roleHierarchy = request.getSecurityAnalyzerInterface();
+        SeerSecurityNode root = securityContextService.createRoleTree(roleHierarchy);
+        SeerSecurityContext seerApiSecurityContext = new SeerSecurityContext(roleHierarchy, root);
 
         // combine security root methods
         Set<SecurityRootMethod> combinedRootMethods = new HashSet<>();
-        for (SeerSecurityContext securityContext : securityContexts) {
-            combinedRootMethods.addAll(securityContext.getSecurityRoots());
+        for (SecurityContextWrapper securityContext : securityContexts) {
+            combinedRootMethods.addAll(securityContext.getSecurity().getSecurityRoots());
         }
 
-        // build controller to controller flow along with security roles
-        // add children based on rest flow
-        // keep only those related to api calls
-        Set<SecurityRootMethod> apiRootMethods = new HashSet<>();
-        for (SecurityRootMethod rootMethod : combinedRootMethods) {
-            Set<String> newChildMethods = new HashSet<>();
+        // get api security root methods
+        Set<SecurityRootMethod> apiRootMethods = generateApiRootMethods(combinedRootMethods, restFlows);
+        seerApiSecurityContext.setSecurityRoots(apiRootMethods);
 
-            for (String child : rootMethod.getChildMethods()) {
-                for (RestFlow restFlow : radResponseContext.getRestFlowContext().getRestFlows()) {
-                    if (child.contains(restFlow.getClassName()) && child.contains(restFlow.getMethodName())) {
-                        // add all servers as child
-                        for (RestEntity server : restFlow.getServers()) {
-                            newChildMethods.add(server.getClassName() + "." + server.getMethodName());
-                        }
-                    }
-                }
-            }
+        // get all api security methods
+        List<SecurityMethod> allSecurityMethods = generateAllApiSecurityMethods(seerApiSecurityContext, combinedRootMethods);
 
-            if (newChildMethods.size() > 0) { // match found
-                rootMethod.setChildMethods(newChildMethods);
-                apiRootMethods.add(rootMethod);
-            }
-        }
+        // find violations
+        List<SecurityMethod> violatingMethods = allSecurityMethods
+                .stream()
+                .filter(x -> x.getRoles().size() > 1)
+                .collect(Collectors.toList());
 
-        // TODO test all security methods
-        String roleHierarchy = request.getSecurityAnalyzerInterface();
-        SeerSecurityNode root = securityContextService.createRoleTree(roleHierarchy);
-        SeerSecurityContext securityContext = new SeerSecurityContext(roleHierarchy, root);
+        Set<SeerSecurityConstraintViolation> roleViolations = securityContextService.findEndpointRoleViolations(seerApiSecurityContext, apiRootMethods);
+        roleViolations.addAll(securityContextService.findFlowViolations(seerApiSecurityContext, violatingMethods));
+        Set<SeerSecurityEntityAccessViolation> entityAccessViolations = securityContextService.findApiViolations(seerApiSecurityContext, apiRootMethods);
 
-        securityContext.setSecurityRoots(apiRootMethods);
+        // seerApiSecurityContext.setRoleViolations(roleViolations);
+        // seerApiSecurityContext.setEntityAccessViolations(entityAccessViolations);
+
+        return new ApiSecurityContext(allSecurityMethods, entityAccessViolations, roleViolations);
+    }
+
+    private List<SecurityMethod> generateAllApiSecurityMethods(SeerSecurityContext securityContext, Set<SecurityRootMethod> combinedRootMethods) {
         Map<String, SecurityMethod> map = securityContextService.buildMap(securityContext);
 
         List<SecurityMethod> allSecurityMethods = new ArrayList<>();
@@ -92,48 +124,35 @@ public class RadAnalysisService {
             }
         }
 
-        responseContext.setAllSecurityMethods(allSecurityMethods);
-
-//        SeerSecurityContext newSecurityContext = securityContextService.getRadSecurityContext(request.getSecurityAnalyzerInterface(), combinedRootMethods, apiRootMethods);
-//
-//        responseContext.setApiSecurityRootMethods(apiRootMethods);
-//        responseContext.setEntityAccessViolations(newSecurityContext.getEntityAccessViolations());
-//        responseContext.setConstraintViolations(newSecurityContext.getRoleViolations());
-
-        List<SecurityMethod> violatingMethods = allSecurityMethods
-                .stream()
-                .filter(x -> x.getRoles().size() > 1)
-                .collect(Collectors.toList());
-
-        Set<SeerSecurityConstraintViolation> roleViolations = securityContextService.findEndpointRoleViolations(securityContext, apiRootMethods);
-        roleViolations.addAll(securityContextService.findFlowViolations(securityContext, violatingMethods));
-        Set<SeerSecurityEntityAccessViolation> entityAccessViolations = securityContextService.findApiViolations(securityContext, apiRootMethods);
-
-        securityContext.setRoleViolations(roleViolations);
-        securityContext.setEntityAccessViolations(entityAccessViolations);
-
-        responseContext.setEntityAccessViolations(securityContext.getEntityAccessViolations());
-        responseContext.setConstraintViolations(securityContext.getRoleViolations());
-
-        return responseContext;
+        return allSecurityMethods;
     }
 
-    private RadResponseContext generateRadResponseContext(RadAnalysisRequestContext request) {
-        return restDiscoveryService.generateRadResponseContext(convertToRadRequestContext(request));
-    }
+    private Set<SecurityRootMethod> generateApiRootMethods(Set<SecurityRootMethod> combinedRootMethods, List<RestFlow> restFlows) {
+        // build controller to controller flow along with security roles
+        // add children based on rest flow
+        // keep only those related to api calls
+        Set<SecurityRootMethod> apiRootMethods = new HashSet<>();
+        for (SecurityRootMethod rootMethod : combinedRootMethods) {
+            Set<String> newChildMethods = new HashSet<>();
 
-    private List<SeerSecurityContext> generateSeerSecurityContext(RadAnalysisRequestContext request) {
-        List<SeerSecurityContext> securityContexts = new ArrayList<>();
+            for (String child : rootMethod.getChildMethods()) {
+                for (RestFlow restFlow : restFlows) {
+                    if (child.contains(restFlow.getClassName()) && child.contains(restFlow.getMethodName())) {
+                        // add all servers as child
+                        for (RestEntity server : restFlow.getServers()) {
+                            newChildMethods.add(server.getClassName() + "." + server.getMethodName());
+                        }
+                    }
+                }
+            }
 
-        List<String> resourcePaths = resourceService.getResourcePaths(request.getPathToCompiledMicroservices());
-        for (String path : resourcePaths) {
-            List<CtClass> ctClasses = resourceService.getCtClasses(path, request.getOrganizationPath());
-            SeerRequestContext seerRequestContext = covertToSeerRequestContext(request);
-            SeerSecurityContext securityContext = securityContextService.getMsSeerSecurityContext(ctClasses, seerRequestContext);
-            securityContexts.add(securityContext);
+            if (newChildMethods.size() > 0) { // match found
+                rootMethod.setChildMethods(newChildMethods);
+                apiRootMethods.add(rootMethod);
+            }
         }
 
-        return securityContexts;
+        return apiRootMethods;
     }
 
     private SeerRequestContext covertToSeerRequestContext(RadAnalysisRequestContext request) {
