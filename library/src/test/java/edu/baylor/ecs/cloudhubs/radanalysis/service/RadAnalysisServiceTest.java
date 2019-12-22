133e3a88fe2484916ef417166da7063dbf240dd2
Dipta Das <dipta670@gmail.com>
2019-12-21 19:15:03 -0600

    Add integration test for library module

---
+++ library/src/test/java/edu/baylor/ecs/cloudhubs/radanalysis/service/RadAnalysisServiceTest.java
@@ -0,0 +1,38 @@
+package edu.baylor.ecs.cloudhubs.radanalysis.service;
+
+import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisRequestContext;
+import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisResponseContext;
+import edu.baylor.ecs.seer.common.security.SeerSecurityConstraintViolation;
+import edu.baylor.ecs.seer.common.security.ViolationType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RadAnalysisServiceTest {
+
+    @Test
+    void generateRadAnalysisResponseContext() {
+        RadAnalysisService radAnalysisService = new RadAnalysisService();
+        RadAnalysisRequestContext requestContext = new RadAnalysisRequestContext(
+                "..",
+                "edu/baylor/ecs/cloudhubs/radanalysis/sample",
+                null,
+                "SuperAdmin \n SuperAdmin->Admin \n SuperAdmin->Reviewer \n Admin->User \n User->Guest \n Admin->Moderator"
+        );
+        RadAnalysisResponseContext responseContext = radAnalysisService.generateRadAnalysisResponseContext(requestContext);
+
+        int countUnrestrictedAccess = 0;
+        int countHierarchy = 0;
+
+        for (SeerSecurityConstraintViolation violation : responseContext.getApiSecurityContext().getConstraintViolations()) {
+            if (violation.getType() == ViolationType.UNRESTRICTED_ACCESS) {
+                countUnrestrictedAccess++;
+            } else if (violation.getType() == ViolationType.HIERARCHY) {
+                countHierarchy++;
+            }
+        }
+
+        assertEquals(countUnrestrictedAccess, 2);
+        assertEquals(countHierarchy, 1);
+    }
+}
