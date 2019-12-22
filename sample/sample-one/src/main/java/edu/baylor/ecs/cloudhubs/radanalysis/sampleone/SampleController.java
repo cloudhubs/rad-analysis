1dd7ec5e4822cdec10f8be6bb29ed993ff334998
Dipta Das <dipta670@gmail.com>
2019-12-21 18:42:47 -0600

    Link controller to service in sample-one

--- sample/sample-one/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampleone/SampleController.java
+++ sample/sample-one/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampleone/SampleController.java
@@ -1,25 +1,29 @@
 package edu.baylor.ecs.cloudhubs.radanalysis.sampleone;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/sample-one")
 public class SampleController {
+    private final SampleService sampleService;
+
     @GetMapping("/p1")
     public SampleModel path1() {
-        return new SampleModel();
+        return sampleService.path1();
     }
 
     @GetMapping("/p2")
     public SampleModel path2() {
-        return new SampleModel();
+        return sampleService.path2();
     }
 
     @GetMapping("/p3")
     public SampleModel path3() {
-        return new SampleModel();
+        return sampleService.path3();
     }
 
 }
