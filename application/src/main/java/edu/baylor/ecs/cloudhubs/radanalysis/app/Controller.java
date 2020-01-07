e57bd0da41563cc953d5a4060c07477cfe3238d8
Dipta Das <dipta670@gmail.com>
2020-01-07 00:25:09 -0600

    Add endpoints for discrete and combined analysis

--- application/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/app/Controller.java
+++ application/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/app/Controller.java
@@ -1,16 +1,23 @@
 package edu.baylor.ecs.cloudhubs.radanalysis.app;
 
+import edu.baylor.ecs.cloudhubs.radanalysis.context.Deployed.CombinedRequestContext;
+import edu.baylor.ecs.cloudhubs.radanalysis.context.Deployed.CombinedResponseContext;
+import edu.baylor.ecs.cloudhubs.radanalysis.context.Deployed.DiscreteRequestContext;
+import edu.baylor.ecs.cloudhubs.radanalysis.context.Deployed.DiscreteResponseContext;
 import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisRequestContext;
 import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisResponseContext;
+import edu.baylor.ecs.cloudhubs.radanalysis.service.DeployedAnalysisService;
 import edu.baylor.ecs.cloudhubs.radanalysis.service.RadAnalysisService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
     private final RadAnalysisService radAnalysisService;
+    private final DeployedAnalysisService deployedAnalysisService;
 
     public Controller() {
         this.radAnalysisService = new RadAnalysisService();
+        this.deployedAnalysisService = new DeployedAnalysisService();
     }
 
     @CrossOrigin(origins = "*")
@@ -18,5 +25,19 @@ public class Controller {
     @ResponseBody
     public RadAnalysisResponseContext getRadResponseContext(@RequestBody RadAnalysisRequestContext request) {
         return radAnalysisService.generateRadAnalysisResponseContext(request);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(path = "/discrete", method = RequestMethod.POST, produces = "application/json; charset=UTF-8", consumes = {"text/plain", "application/*"})
+    @ResponseBody
+    public DiscreteResponseContext getDiscreteResponseContext(@RequestBody DiscreteRequestContext request) {
+        return deployedAnalysisService.generateDiscreteResponseContext(request);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(path = "/combined", method = RequestMethod.POST, produces = "application/json; charset=UTF-8", consumes = {"text/plain", "application/*"})
+    @ResponseBody
+    public CombinedResponseContext getCombinedResponseContext(@RequestBody CombinedRequestContext request) {
+        return deployedAnalysisService.generateCombinedResponseContext(request);
     }
 }
