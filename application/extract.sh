80138b4d3212ca8c37d4a8287f9fd1d5b7d51aff
Dipta Das <dipta670@gmail.com>
2020-01-08 15:09:39 -0600

    Use bash fail checking option instead of explicit checks

--- application/extract.sh
+++ application/extract.sh
@@ -6,8 +6,6 @@ mkdir /target
 
 TARGET_JAR=$(docker inspect $1 | jq -r '.[0].Config.Entrypoint[-1]')
 
-[[ -z "$TARGET_JAR" ]] && { echo "Image or JAR not found" ; exit 1; }
-
 CONTAINER=$(docker create -ti $1 sh)
 docker cp $CONTAINER:${TARGET_JAR} /target/target.jar
 docker rm -f $CONTAINER
