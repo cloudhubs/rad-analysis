f352759000960dd2ae2602a5047bb70d04e1b45a
Dipta Das <dipta670@gmail.com>
2020-01-08 15:08:18 -0600

    Removed fixed name for dummy container

--- application/extract.sh
+++ application/extract.sh
@@ -1,10 +1,13 @@
-set -x
+#!/bin/bash
+set -euxo pipefail
 
+rm -rf /target
 mkdir /target
 
 TARGET_JAR=$(docker inspect $1 | jq -r '.[0].Config.Entrypoint[-1]')
 
-docker create -ti --name dummy $1 bash
-docker cp dummy:${TARGET_JAR} /target/target.jar
-docker rm -f dummy
+[[ -z "$TARGET_JAR" ]] && { echo "Image or JAR not found" ; exit 1; }
 
+CONTAINER=$(docker create -ti $1 sh)
+docker cp $CONTAINER:${TARGET_JAR} /target/target.jar
+docker rm -f $CONTAINER
