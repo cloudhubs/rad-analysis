a2598c1024c62a0af22f5b0497801e7f4fa53a92
Dipta Das <dipta670@gmail.com>
2020-01-07 23:07:44 -0600

    POJO for sample model

--- sample/sample-one/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampleone/SampleModel.java
+++ sample/sample-one/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampleone/SampleModel.java
@@ -1,5 +1,14 @@
 package edu.baylor.ecs.cloudhubs.radanalysis.sampleone;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SampleModel {
     Long id;
     String name;
--- sample/sample-two/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampletwo/SampleModel.java
+++ sample/sample-two/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampletwo/SampleModel.java
@@ -1,5 +1,14 @@
 package edu.baylor.ecs.cloudhubs.radanalysis.sampletwo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SampleModel {
     Long id;
     String name;
