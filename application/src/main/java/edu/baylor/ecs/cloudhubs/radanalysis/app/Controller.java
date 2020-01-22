7fc46a862836eb405c49695a1ef234019d475cea
Dipta Das <dipta670@gmail.com>
2020-01-22 01:07:36 -0600

    List pods and images for services in default namespace

--- application/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/app/Controller.java
+++ application/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/app/Controller.java
@@ -5,6 +5,15 @@ import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisRequestContext;
 import edu.baylor.ecs.cloudhubs.radanalysis.context.RadAnalysisResponseContext;
 import edu.baylor.ecs.cloudhubs.radanalysis.service.DeployedAnalysisService;
 import edu.baylor.ecs.cloudhubs.radanalysis.service.RadAnalysisService;
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.models.V1Service;
+import io.kubernetes.client.models.V1ServiceList;
+import io.kubernetes.client.util.Config;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +23,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class Controller {
@@ -84,6 +95,48 @@ public class Controller {
 
 
         return deployedAnalysisService.generateCombinedResponseContext(new CombinedRequestContext(securityInterface, discreteResponseContexts));
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(path = "/artifacts", method = RequestMethod.GET, produces = "application/json; charset=UTF-8", consumes = {"text/plain", "application/*"})
+    @ResponseBody
+    public AnalysisRequestContext getDeployedArtifacts() throws ApiException, IOException {
+        List<DiscreteArtifact> discreteArtifacts = new ArrayList<>();
+
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        String namespace = "default";
+
+        V1ServiceList svcList = api.listNamespacedService(
+                namespace, null, null, null, null,
+                null, null, null, null, null);
+
+
+        for (V1Service service : svcList.getItems()) {
+            String labelSelector = getLabelSelector(service.getSpec().getSelector());
+
+            V1PodList podList = api.listNamespacedPod(namespace, null, null, null, null,
+                    labelSelector, null, null, null, null);
+
+            for (V1Pod pod : podList.getItems()) {
+                discreteArtifacts.add(new DiscreteArtifact(
+                        service.getMetadata().getName(),
+                        pod.getSpec().getContainers().get(0).getName()));
+            }
+        }
+
+        return new AnalysisRequestContext(discreteArtifacts);
+    }
+
+    private String getLabelSelector(Map<String, String> selectors) {
+        if (selectors == null) return null;
+
+        String labelSelector = selectors.keySet().stream()
+                .map(key -> key + "=" + selectors.get(key))
+                .collect(Collectors.joining(","));
+        return labelSelector;
     }
 
     private void runExtractScript(String dockerImage) throws InterruptedException, IOException {
