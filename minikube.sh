263cc01f95cd4572b35c17f7addb83735fcd6f50
Dipta Das <dipta670@gmail.com>
2020-01-27 11:37:39 -0600

    Add scripts to build and deploy

---
+++ minikube.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euxo pipefail
+
+mvn clean install -DskipTests
+
+pushd application
+docker build -t diptadas/rad-analysis .
+popd
+
+pushd sample/sample-one
+docker build -t diptadas/rad-sample-one .
+popd
+
+pushd sample/sample-two
+docker build -t diptadas/rad-sample-two .
+popd
+
+minikube start
+
+kubectl delete -f application/deploy.yaml || true
+kubectl delete -f sample/sample-one/deploy.yaml || true
+kubectl delete -f sample/sample-two/deploy.yaml || true
+
+minducker diptadas/rad-analysis
+minducker diptadas/rad-sample-one
+minducker diptadas/rad-sample-two
+
+kubectl apply -f application/deploy.yaml
+kubectl apply -f sample/sample-one/deploy.yaml
+kubectl apply -f sample/sample-two/deploy.yaml
