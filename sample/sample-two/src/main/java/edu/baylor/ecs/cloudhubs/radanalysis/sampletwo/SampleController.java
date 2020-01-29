d201b84541b8fae85b73e744a2144dffde7f999f
Dipta Das <dipta670@gmail.com>
2020-01-29 12:53:52 -0600

    Update node ports

--- sample/sample-two/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampletwo/SampleController.java
+++ sample/sample-two/src/main/java/edu/baylor/ecs/cloudhubs/radanalysis/sampletwo/SampleController.java
@@ -12,19 +12,19 @@ public class SampleController {
     @RolesAllowed("admin")
     @GetMapping("/p1")
     public SampleModel path1() {
-        return new SampleModel();
+        return new SampleModel(1L, "path01");
     }
 
     @RolesAllowed("admin")
     @GetMapping("/p2")
     public SampleModel path2() {
-        return new SampleModel();
+        return new SampleModel(2L, "path02");
     }
 
     @RolesAllowed("admin")
     @GetMapping("/p3")
     public SampleModel path3() {
-        return new SampleModel();
+        return new SampleModel(3L, "path03");
     }
 
 }
